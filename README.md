1bbc3a21b63c71feb65af398a09a26b8b757cc4e
unknown <ilkercankaya@sabanciuniv.edu>
2019-01-19 17:35:45 +0300

    Web scrapping in both web and file w/ writing to a CSV file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Web-Scraping
-An simple script to web scrape in Python with BeautifulSoup
+# Web And File Scraping
+An simple script to web scrape in _**Python**_ with **_BeautifulSoup4._**
+
+# How It Works
+This script scrapes:
+* index.html 
+* Correy's own [website.](http://coreyms.com/) 
+
+and creates a csv file with the information scraped.
+
+# Script Features
+* Basic scraping and printing information of both an html file and a website to the console.
+* Error handling such as empty missing information during scraping.
+* Writing the scraped information as a csv file.
+
+# Script Details
+Technologies involved:
+* _Python 3.7_
+* _BeautifulSoup4_ - Library for Python
+* _Requests_ - Library for Python
+* _CSV_ - Library for Python
+
+# Files
+* `scraping.py`: Demonstration of basic scraping
+* `corey.csv`: CSV file of the scraped information of the _**website**_
+* `index.csv`: CSV file of the scraped information of the file _**index.html**_
+
+# Results
+Check the [corey.csv](/corey.csv) and [index.csv](/index.csv) files!
--- scraping.py
+++ scraping.py
@@ -1,5 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+
+# Manage CSV file, Writer
+
+csv_file = open("corey.csv", 'w')
+csv_file_index = open("index.csv", 'w')
+
+# Corey writer
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Headline', 'Summary', 'Video_link'])
+
+csv_writer_index = csv.writer(csv_file_index)
+csv_writer_index.writerow(['Headline', 'Summary', "Links"])
 
 # Scapes index.html
 with open('index.html') as html_file:
@@ -19,14 +32,64 @@ with open('index.html') as html_file:
 # print(match)
 
 # Gets all the divs that are article class
-# match = soup.find_all('div', class_='article')
+match = soup.find_all('div', class_='article')
 # See the ref of all pages in article
-# for article in match:
-#     print(article.h2.a["href"])
+for article in match:
+    headline = article.h2.text
+    summary = article.p.text
+    link = article.h2.a["href"]
 
+    # Append to CSV file
+    csv_writer_index.writerow([headline, summary, link])
 
+# Scapes coreyms.com
+URL = "http://coreyms.com/"
+# Text gets the HTML text
+source = requests.get(URL).text
+soup_corey = BeautifulSoup(source, 'lxml')
 
+# See the page
+# print(soup_corey.prettify())
 
+# Find the first articles
+article_first = soup_corey.find('article')
+# print(article_first.prettify())
+# summary = article_first.find('div', class_="entry-content").p.text
+# print(summary)
 
+# Get the youtube link
+vid_source = article_first.find('iframe', class_="youtube-player")['src']
+# print(vid_source)
 
+vid_id = vid_source.split('/')[4]
+# print(vid_id)
 
+vid_id = vid_id.split('?')[0]
+# print(vid_id)
+
+# F is available over python 3.6
+yt_link = f"https://www.youtube.com/watch?v={vid_id}"
+# print(yt_link)
+
+articles = soup_corey.find_all('article')
+# print(len(articles))
+for article in articles:
+    try:
+        # Get parapgraph
+        headline = article.h2.a.text
+        summary = article.find('div', class_="entry-content").p.text
+        # Get the youtube link
+        vid_source = article.find('iframe', class_="youtube-player")['src']
+        vid_id = vid_source.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+
+        # F is available over python 3.6
+        yt_link = f"https://www.youtube.com/watch?v={vid_id}"
+    except (AttributeError, KeyError) as ex:
+        pass
+        # or make the information None: yt_link = None
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
+print("Finished Scraping both file and website!")
